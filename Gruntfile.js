4327339cb4a3f483c5eb60b8d9755bd48871e2c3
LeonPeters <petersleon12@gmail.com>
2021-01-02 11:42:38 +0100

    Grunt Part 2 (preparing distribution folder)

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,9 @@ module.exports = function(grunt) { //how node modules are defined
     require('time-grunt')(grunt); //load in the time grunt node module to be used in this particular node module
     
     //Automatically load required Grunt tasks
-    require('jit-grunt')(grunt); //will load in any other node modules or grunt plug-ins, when they are implied implicitely within Grunt
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    }); //will load in any other node modules or grunt plug-ins, when they are implied implicitely within Grunt
     
     //Define the configuration for all the tasks
     grunt.initConfig({
@@ -38,10 +40,147 @@ module.exports = function(grunt) { //how node modules are defined
                     }
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            font: {
+                files:[{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src:['font/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean:{
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: './',                   // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['contactus.html','aboutus.html','index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js:['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function (context, block) {
+                            var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialComments: 0, rebase: false
+                                };
+                            }       
+                        }]
+                    }
+                }
+            }
+        },
+
+        // Concat
+        concat: {
+            options: {
+                separator: ';'
+            },
+  
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        // Uglify
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+
+        cssmin: {
+            dist: {}
+        },
+
+        // Filerev (file revision)
+        // add additional extension to main files to indicate what revision version the current files are
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+  
+            release: {
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+        },
+  
+        // Usemin
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/contactus.html','dist/aboutus.html','dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css','dist/js']
+            }
+        },
+
+        htmlmin: {                                         // Task
+            dist: {                                        // Target
+                options: {                                 // Target options
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'dist/index.html',  // 'destination': 'source'
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
+                }
+            }
         }
     });
 
     //This task does the same as previously assigning watch task to watch SCSS files
     grunt.registerTask('css', ['sass']); //task name is CSS, task involves executing the SASS tasks which was already configured. It can be run in CMD with 'grunt css'
     grunt.registerTask('default', ['browserSync', 'watch'])
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ]);
 };
